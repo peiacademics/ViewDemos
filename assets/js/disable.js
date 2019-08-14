aff98784ea2d02a803a1a294c9ccfd7a06a3637b
pedeveloper2019 <pedeveloper.2019@gmail.com>
2019-08-14 18:51:13 +0530

    .

--- assets/js/disable.js
+++ assets/js/disable.js
@@ -2,4 +2,9 @@ $(function () {
     $("img").on("contextmenu",function(){
         return false;
      }); 
+     window.oncontextmenu = function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+   };
 });
